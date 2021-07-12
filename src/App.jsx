6b0d8effbbfa0943037081f1a060e7ec7878d1bb
Michael Wanyoike <micwanyoike@gmail.com>
2021-07-12 22:22:22 +0300

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <header>Navbar</header>
+      <main>
+        <h1>Hello</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
