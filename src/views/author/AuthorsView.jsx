d8b64f35b2140eca23de05845e5713fa771abfbc
Michael Wanyoike <micwanyoike@gmail.com>
2021-07-17 15:57:45 +0300

    link author and category list to views

--- src/views/author/AuthorsView.jsx
+++ src/views/author/AuthorsView.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PageHeading from '../../components/ui/PageHeading'
+import AuthorList from '../../components/author/AuthorList'
 
 function AuthorsView() {
   return (
     <>
       <PageHeading title="Author List" />
+      <AuthorList />
     </>
   )
 }
--- src/views/category/CategoriesView.jsx
+++ src/views/category/CategoriesView.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PageHeading from '../../components/ui/PageHeading'
+import CategoryList from '../../components/category/CategoryList'
 
 function CategoriesView() {
   return (
     <>
       <PageHeading title="Category List" />
+      <CategoryList />
     </>
   )
 }
