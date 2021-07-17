a4fbe963cc26b715ee22c63d5355c7636badc1ea
Michael Wanyoike <micwanyoike@gmail.com>
2021-07-17 15:32:28 +0300

    Desig home page

--- src/components/ui/PageHeading.jsx
+++ src/components/ui/PageHeading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PageHeading({ title }) {
   return (
-    <div className="pb-5 border-b border-accent">
+    <div className="pb-5 mt-4 border-b border-accent">
       <h1 className="leading-6">{title}</h1>
     </div>
   )
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import {
+  CollectionIcon,
+  UserCircleIcon,
+  BookOpenIcon,
+} from '@heroicons/react/outline'
 import PageHeading from '../components/ui/PageHeading'
 
+function PageCardLink({ title, url, icon }) {
+  return (
+    <Link
+      className="flex flex-col w-48 p-4 place-items-center bg-neutral rounded-box hover:text-primary"
+      to={url}
+    >
+      {React.createElement(icon, {
+        className: 'h-36 w-36',
+        'aria-hidden': 'true',
+      })}
+
+      <h3 className="text-lg font-bold">{title} </h3>
+    </Link>
+  )
+}
+
 function Home() {
+  const data = [
+    {
+      title: 'Category',
+      url: '/category/list',
+      icon: CollectionIcon,
+    },
+    {
+      title: 'Author',
+      url: '/author/list',
+      icon: UserCircleIcon,
+    },
+    {
+      title: 'Book',
+      url: '/book/list',
+      icon: BookOpenIcon,
+    },
+  ]
+
+  const pageCards = data.map((item, index) => (
+    <PageCardLink
+      key={index}
+      title={item.title}
+      url={item.url}
+      icon={item.icon}
+    />
+  ))
   return (
     <>
       <PageHeading title="Home" />
       <h2 className="mt-8 text-lg font-semibold text-primary">
         Browser books by:
       </h2>
-      <div>
-        <Link to="/category/list">Category</Link>
-        <Link to="/author/list">Author</Link>
-        <Link to="/book/list">Book</Link>
+      <div className="grid max-w-screen-md grid-cols-1 gap-4 mt-8 justify-items-center md:grid-cols-3 md:justify-items-start">
+        {pageCards}
       </div>
     </>
   )
