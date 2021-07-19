e92b1c2c65487752ac49e9b3ba9c9d82e5e3c534
Michael Wanyoike <micwanyoike@gmail.com>
2021-07-19 16:06:13 +0300

    partial alert implementation

--- src/components/ui/Alert.jsx
+++ src/components/ui/Alert.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+/* This example requires Tailwind CSS v2.0+ */
+import { CheckCircleIcon, XIcon } from '@heroicons/react/solid'
 
-function Alert() {
+export default function Alert() {
   return (
-    <div>
-      <p>Alert</p>
+    <div className="p-4 rounded-md bg-green-50">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <CheckCircleIcon
+            className="w-5 h-5 text-green-400"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="ml-3">
+          <p className="text-sm font-medium text-green-800">
+            Successfully uploaded
+          </p>
+        </div>
+        <div className="pl-3 ml-auto">
+          <div className="-mx-1.5 -my-1.5">
+            <button
+              type="button"
+              className="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
+            >
+              <span className="sr-only">Dismiss</span>
+              <XIcon className="w-5 h-5" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
-export default Alert
