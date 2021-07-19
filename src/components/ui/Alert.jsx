3317a256213c140eb4cfc8aba5c83a167c58b925
Michael Wanyoike <micwanyoike@gmail.com>
2021-07-19 16:55:36 +0300

    Refactor Alert UI

--- src/components/ui/Alert.jsx
+++ src/components/ui/Alert.jsx
@@ -1,33 +1,29 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { CheckCircleIcon, XIcon } from '@heroicons/react/solid'
+import React from 'react'
+import {
+  CheckCircleIcon,
+  BanIcon,
+  ExclamationCircleIcon,
+  InformationCircleIcon,
+} from '@heroicons/react/outline'
 
-export default function Alert() {
+function Alert({ type, message, innerClass }) {
+  const icons = {
+    info: InformationCircleIcon,
+    success: CheckCircleIcon,
+    warning: ExclamationCircleIcon,
+    error: BanIcon,
+  }
   return (
-    <div className="p-4 rounded-md bg-green-50">
-      <div className="flex">
-        <div className="flex-shrink-0">
-          <CheckCircleIcon
-            className="w-5 h-5 text-green-400"
-            aria-hidden="true"
-          />
-        </div>
-        <div className="ml-3">
-          <p className="text-sm font-medium text-green-800">
-            Successfully uploaded
-          </p>
-        </div>
-        <div className="pl-3 ml-auto">
-          <div className="-mx-1.5 -my-1.5">
-            <button
-              type="button"
-              className="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600"
-            >
-              <span className="sr-only">Dismiss</span>
-              <XIcon className="w-5 h-5" aria-hidden="true" />
-            </button>
-          </div>
-        </div>
+    <div className={`alert alert-${type}`}>
+      <div className={`flex-1 ${innerClass}`}>
+        {React.createElement(icons[type], {
+          className: 'w-6 h-6 mx-2',
+          'aria-hidden': 'true',
+        })}
+        <label>{message}</label>
       </div>
     </div>
   )
 }
+
+export default Alert
