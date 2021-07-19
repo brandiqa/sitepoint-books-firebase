53d05583cb3902b5732fc7b5e18db89a988e0bd8
Michael Wanyoike <micwanyoike@gmail.com>
2021-07-19 17:29:50 +0300

    fix firebase config env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_API_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_API_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_API_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_API_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_API_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_API_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_API_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_API_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_API_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_API_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_API_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_API_FIREBASE_APP_ID,
 }
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+const app = firebase.initializeApp(firebaseConfig)
 
 export const auth = app.auth()
+export const db = app.firestore()
 export default app
