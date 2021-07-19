e0a6de3830c2bfaa99dd1cd55582348b8021864b
Michael Wanyoike <micwanyoike@gmail.com>
2021-07-19 17:31:51 +0300

    implement fetch all authors

--- src/screens/author/List.jsx
+++ src/screens/author/List.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import { db } from '../../firebase'
 import PageHeading from '../../components/ui/PageHeading'
 import AuthorList from '../../components/author/List'
+import Alert from '../../components/ui/Alert'
 
 function ScreenAuthorList() {
-  const authors = [
-    { id: '123', name: 'Jane Cooper' },
-    { id: '124', name: 'Cody Fisher' },
-  ]
+  const authorsRef = db.collection('authors')
+
+  const fetchAuthors = async () => {
+    const snapshot = await authorsRef.get()
+    return snapshot.docs.map((doc) => {
+      const data = doc.data()
+      data.id = doc.id
+      return data
+    })
+  }
+
+  const { data, isLoading, isError, error, status } = useQuery(
+    'authors',
+    fetchAuthors
+  )
 
   return (
     <>
       <PageHeading title="Author List" />
       <div className="mt-12">
-        <AuthorList data={authors} />
+        {isError && <Alert type="error" message={error.message} />}
+        {isLoading && (
+          <Alert
+            type="info"
+            message="Loading..."
+            innerClass="animate animate-pulse"
+          />
+        )}
+        {status === 'success' && <AuthorList data={data} />}
       </div>
     </>
   )
