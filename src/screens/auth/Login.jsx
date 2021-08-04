a4b10d739d39f4e5055f7ea916e72b42a2aba002
Michael Wanyoike <micwanyoike@gmail.com>
2021-08-05 00:03:20 +0300

    refactor screen login with daisy ui components

--- src/screens/auth/Login.jsx
+++ src/screens/auth/Login.jsx
@@ -8,43 +8,35 @@ function ScreenLogin() {
       <div className="flex flex-col justify-center py-12 bg-base sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-3xl font-extrabold text-center">
-            Admin Login
+            User Login
           </h2>
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="px-4 py-8 shadow bg-base-200 sm:rounded-lg sm:px-10">
             <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium">
-                  Email address
+              <div className="form-control">
+                <label className="label" htmlFor="email">
+                  <span className="label-text">Email address</span>
                 </label>
-                <div className="mt-1">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
+                <input
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="input"
+                />
               </div>
 
-              <div>
-                <label htmlFor="password" className="block text-sm font-medium">
-                  Password
+              <div className="form-control">
+                <label className="label" htmlFor="password">
+                  <span className="label-text">Password</span>
                 </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
+                <input
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="input"
+                />
               </div>
 
               <div className="flex items-center justify-between">
