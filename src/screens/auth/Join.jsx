96c53e9d688b135b1d692790a440263205004e19
Michael Wanyoike <micwanyoike@gmail.com>
2021-08-05 02:00:51 +0300

    join screen form logic

--- src/screens/auth/Join.jsx
+++ src/screens/auth/Join.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
 import PageHeading from '../../components/ui/PageHeading'
+import { useForm } from 'react-hook-form'
+import Alert from '../../components/ui/Alert'
+
+const schema = yup.object().shape({
+  email: yup.string().label('Email').required().email(),
+  password: yup.string().label('Password').required().min(8),
+  passwordConfirm: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match'),
+})
 
 function ScreenJoin() {
+  const [message, setMessage] = useState('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data) => {
+    setMessage('Feature not yet available')
+    console.log(data)
+  }
+
   return (
     <>
       <PageHeading title="Join Sitepoint Books" />
@@ -12,33 +39,65 @@ function ScreenJoin() {
             Create User Account
           </h2>
         </div>
+
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="px-4 py-8 shadow bg-base-200 sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+              {message && <Alert type="error" message={message} />}
+
               <div className="form-control">
                 <label className="label" htmlFor="email">
                   <span className="label-text">Email address</span>
                 </label>
                 <input
                   type="email"
-                  autoComplete="email"
-                  required
-                  className="input"
+                  autoComplete="off"
+                  className={`input input-bordered ${
+                    errors.email && 'input-error'
+                  }`}
+                  {...register('email')}
                 />
+                {errors.email && (
+                  <span className="mt-1 text-xs text-error">
+                    {errors.email.message}
+                  </span>
+                )}
               </div>
 
               <div className="form-control">
                 <label className="label" htmlFor="password">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" required className="input" />
+                <input
+                  type="password"
+                  className={`input input-bordered ${
+                    errors.password && 'input-error'
+                  }`}
+                  {...register('password')}
+                />
+                {errors.password && (
+                  <span className="mt-1 text-xs text-error">
+                    {errors.password.message}
+                  </span>
+                )}
               </div>
 
               <div className="form-control">
                 <label className="label" htmlFor="passwordConfirm">
                   <span className="label-text">Password Confirmation</span>
                 </label>
-                <input type="password" required className="input" />
+                <input
+                  type="password"
+                  className={`input input-bordered ${
+                    errors.passwordConfirm && 'input-error'
+                  }`}
+                  {...register('passwordConfirm')}
+                />
+                {errors.passwordConfirm && (
+                  <span className="mt-1 text-xs text-error">
+                    {errors.passwordConfirm.message}
+                  </span>
+                )}
               </div>
 
               <div>
