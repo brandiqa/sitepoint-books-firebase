f7349cc08ac497b46e77e06f110b10eb133ec05d
Michael Wanyoike <micwanyoike@gmail.com>
2021-08-05 15:16:13 +0300

    implement author form ui

--- src/components/author/Form.jsx
+++ src/components/author/Form.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import Alert from '../../components/ui/Alert'
+
+const schema = yup.object().shape({
+  name: yup.string().label('Name').required().min(2),
+})
 
 function AuthorForm() {
+  const [successMsg, setSuccessMsg] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data) => {
+    try {
+      console.log(data)
+    } catch (err) {
+      setErrorMsg(err.message)
+    }
+  }
   return (
-    <div>
-      <p>Author Form</p>
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+        {errorMsg && <Alert type="error" message={errorMsg} />}
+
+        {successMsg && <Alert type="success" message={successMsg} />}
+
+        <div className="form-control">
+          <label className="label" htmlFor="name">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            autoComplete="off"
+            {...register('name')}
+            className={`input input-bordered ${errors.name && 'input-error'}`}
+          />
+          {errors.name && (
+            <span className="mt-1 text-xs text-error">
+              {errors.name.message}
+            </span>
+          )}
+        </div>
+
+        <div className="flex justify-end space-x-4">
+          <button type="submit" className="btn btn-primary btn-sm w-24">
+            Save
+          </button>
+          <button type="link" className="btn btn-outline btn-sm w-24">
+            Cancel
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
