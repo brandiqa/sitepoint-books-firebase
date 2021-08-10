f3be0ff49d697aacf431de68943635967f55f53f
Michael Wanyoike <micwanyoike@gmail.com>
2021-08-11 02:34:56 +0300

    Book Card UI

--- src/components/book/Card.jsx
+++ src/components/book/Card.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { PencilAltIcon } from '@heroicons/react/outline'
+import StorageService from '../../services/StorageService'
 
-function BookCard() {
+function BookCard({ book }) {
+  const [coverURL, setCoverURL] = useState()
+
+  useEffect(async () => {
+    const url = await StorageService.getImageURL(book.cover)
+    setCoverURL(url)
+  }, [book])
+
   return (
     <div>
-      <p>Book Card</p>
+      <Link to={`/book/detail/${book.id}`}>
+        <img src={coverURL} alt={book.title} />
+      </Link>
+      <Link to={`/book/edit/${book.id}`} title="Edit Book">
+        <PencilAltIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
+      </Link>
     </div>
   )
 }
