76182c68c3d4db9291f3554557a7582b4cb6d0e2
Michael Wanyoike <micwanyoike@gmail.com>
2021-08-11 13:02:17 +0300

    Removed Book Form and Edit Link

--- src/components/book/Card.jsx
+++ src/components/book/Card.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { PencilAltIcon } from '@heroicons/react/outline'
 import StorageService from '../../services/StorageService'
 
 function BookCard({ book }) {
@@ -15,9 +14,6 @@ function BookCard({ book }) {
     <div>
       <Link to={`/book/detail/${book.id}`}>
         <img src={coverURL} alt={book.title} />
-      </Link>
-      <Link to={`/book/edit/${book.id}`} title="Edit Book">
-        <PencilAltIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
       </Link>
     </div>
   )
